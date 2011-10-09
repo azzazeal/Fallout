821c0059143674068e0969a259e78504776016df
Mariusz <azzazeal@tlen.pl>
2011-10-09 21:37:16 +0200

    poczatek

---
+++ karta.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+  printf("\n\n\t FALLOUT \n\n");
+    
+float s1,s2,s3,s4,s5,s6,s7;
+float n1;
+	puts("Siła:"); scanf("%f", &s1);
+	puts("Percepcja:"); scanf("%f", &s2);
+	puts("Wytrzymałość:"); scanf("%f", &s3);
+	puts("Charyzma:"); scanf("%f", &s4);
+	puts("Inteligencja:"); scanf("%f", &s5);
+	puts("Zręczność:"); scanf("%f", &s6);
+	puts("Szczęście:"); scanf("%f", &s7);
+	n1=15+s1+s3*2;
+printf("HP: %2.0f",n1);
+	p
+}
