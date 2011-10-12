b1ebbf72e07bf78ba9ecea6c66636200c3002955
Mariusz <azzazeal@tlen.pl>
2011-10-12 19:40:21 +0200

    ver0.1

--- karta.c
+++ karta.c
@@ -2,16 +2,22 @@
 void main(){
   printf("\n\n\t FALLOUT \n\n");
     
-float s1,s2,s3,s4,s5,s6,s7;
-float n1;
-	puts("Siła:"); scanf("%f", &s1);
-	puts("Percepcja:"); scanf("%f", &s2);
-	puts("Wytrzymałość:"); scanf("%f", &s3);
-	puts("Charyzma:"); scanf("%f", &s4);
-	puts("Inteligencja:"); scanf("%f", &s5);
-	puts("Zręczność:"); scanf("%f", &s6);
-	puts("Szczęście:"); scanf("%f", &s7);
-	n1=15+s1+s3*2;
-printf("HP: %2.0f",n1);
-	p
+float s1,s2,s3,s4,s5,s6,s7,n1;
+	printf("ST:"); 
+	scanf("%f", &s1);
+	printf("PE:"); 
+	scanf("%f", &s2);
+	printf("EN:"); 
+	scanf("%f", &s3);
+	printf("CH:"); 
+	scanf("%f", &s4);
+	printf("IN:"); 
+	scanf("%f", &s5);
+	printf("AG:"); 
+	scanf("%f", &s6);
+	printf("LK:"); 
+	scanf("%f", &s7);
+	n1=15+(s1+s3*2);
+	printf("HP: %2.0f",n1);
+
 }
